396f3af9a59ccaa80b8ccc30fd33270b95180e3e
hydewww <2208493266@qq.com>
2017-11-01 23:09:03 +0800

    change port

--- manage.py
+++ manage.py
@@ -2,18 +2,20 @@
 import os
 from app import create_app, db
 from app.models import User, Team, Flag, Solve, Chal, Role
-from flask_script import Manager, Shell
+from flask_script import Manager, Shell, Server
 from flask_migrate import Migrate, MigrateCommand
 
 app = create_app()
 manager = Manager(app)
 migrate = Migrate(app, db)
+server = Server(port=5001)
 
 
 def make_shell_context():
     return dict(app=app, db=db, User=User, Team=Team, Flag=Flag, Solve=Solve, Chal=Chal)
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
+manager.add_command('runserver', server)
 
 if __name__ == '__main__':
     manager.run()
